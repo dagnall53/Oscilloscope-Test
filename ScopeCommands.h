5170fb6882acb3e0b199e7810f8b5f4a36e1f0ef
dagnall <dagnall53@googlemail.com>
2022-10-20 16:10:15 +0100

    Initial Uploads
    Working with NodeMCU dig D1,D2 and analog A0 ports

---
+++ ScopeCommands.h
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsServer.h>
+#include <Hash.h>
+#include "I2CCommands.h"
+#include "miniDB.h"
+
+
+void SetDigInputs(byte D0 , byte D1);
+void scopeInit(void);
+
+void scopeHandler(WebSocketsServer &WEBSOCKETOBJECT);
+
+void ADCInit(void);
+void setADCChannel(int CHANNEL);
+int ADCRead(void);
+void ADCHandler(byte chan);
+void ScalesInit(byte Data,byte Clock);
+boolean ScalesConnected(void);
+void SetScalesConnected(boolean set);
+long readScales(byte Chan);
+
