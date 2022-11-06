1d0ae0dbe3b7f76395f46b7a02d3d634f2f0cb53
dagnall <dagnall53@googlemail.com>
2022-11-06 11:03:52 +0000

    Minimised #includes for esp32 upgrade

--- I2CCommands.h
+++ I2CCommands.h
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#ifdef ESP32  
-  #include <WiFi.h>
- #else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266WiFiMulti.h>
-  #include <Hash.h>
-  #include <ESP8266WebServer.h>
-  #include <ESP8266mDNS.h>
-#endif
+// #ifdef ESP32  
+//   #include <WiFi.h>
+//  #else
+//   #include <ESP8266WiFi.h>
+//   #include <ESP8266WiFiMulti.h>
+//   #include <Hash.h>
+//   #include <ESP8266WebServer.h>
+//   #include <ESP8266mDNS.h>
+// #endif
 
 #include <WebSocketsServer.h>
 
